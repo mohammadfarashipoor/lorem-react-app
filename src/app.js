614cf880df98a7f91e9cd9ea0d1fa8b8a57dfe55
mohammad <77319747+mohammadfarashipoor@users.noreply.github.com>
2022-01-04 07:37:47 +0330

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,50 @@
+import React ,{useState} from "react";
+import datafa from "./dataText/textfa";
+import dataen from "./dataText/texten";
+
+function App() {
+    const [count,setCount] = useState(0);
+    const [text, setText] = useState([]);
+    const [language, setLanguage] = useState("English");
+    const handlesubmit =(e) =>{
+        e.preventDefault();
+        let amount = parseInt(count);
+        const text = language === "English" ?  dataen : datafa;
+        setText(text.slice(0,amount));
+    }
+    const onLangChange = (e) =>{
+        setLanguage(e.target.value);
+    }
+    
+      
+    
+    return (
+        <>
+        <section className="mx-auto">
+            
+            <form className="lorem-form" onSubmit={handlesubmit}>
+            <h1 >Genrated texis</h1>
+            <label htmlFor="amount">
+                Paragraphs:
+            </label>
+            <input type="number" name="amount" id="amount" value={count} onChange={ (e)=>setCount(e.target.value)}></input>
+            <div onClick={onLangChange}>
+                <input type="radio" id="lan lan-fa" value="Farsi"  name="language" ></input>
+                <label for="Farsi">Farsi</label>
+                <input type="radio" id="lan lan-en" value="English"  name="language" ></input>
+                <label for="English">English</label>
+            </div>
+            <button type="submit" className="btn">Genrated</button>
+            <article className="lorem-text">
+                {text.map((item,index)=>{
+                    return <p key={index}>{item}</p>
+                })}
+            </article>
+            </form>
+
+        </section>
+        </>
+    );
+}
+
+export default App;
